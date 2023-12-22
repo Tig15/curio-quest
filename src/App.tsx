0fb5fe82dd980f26cfaf3a40582e25d35bf27273
Tig15 <amanvgec131@gmail.com>
2023-12-22 18:08:23 +0530

    OneSignal added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import WelcomeScreen from './screen/WelcomeScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './navigation/appNavigator';
 import firebase from '@react-native-firebase/app';
 import firebaseConfig from './firebaseConfig';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
+
+//appid:5fd43638-9717-4ee1-b5e8-6f720a677b36
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
@@ -13,6 +16,19 @@ if (!firebase.apps.length) {
 }
 
 const App = () => {
+  useEffect(() => {
+    const oneSignal_App_Id = '5fd43638-9717-4ee1-b5e8-6f720a677b36';
+    OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+    OneSignal.initialize(oneSignal_App_Id);
+
+    OneSignal.Notifications.requestPermission(true);
+
+    OneSignal.Notifications.addEventListener('click', event => {
+      console.log('OneSignal: notification clicked:', event);
+    });
+  }, []);
+
   return (
     <NavigationContainer>
       <AppNavigator />
